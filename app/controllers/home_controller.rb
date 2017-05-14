2a6efb02a6e46c744bfa1c40be6259b3eb466f3d
Joseph Murphy <josephmurphy@Josephs-MacBook-Pro.local>
2017-05-14 01:01:39 -0500

    finish fixing all the 404 errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @jobs = Experience.all
+    @works_recent = Portfolio.last(2)
     @works = Portfolio.last(9)
     @blogs = Blog.last(2)
 
