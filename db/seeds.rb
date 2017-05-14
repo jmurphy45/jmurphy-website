095b212d8f5c1de2e834fadba60c14fb371630e6
Joseph Murphy <josephmurphy@Josephs-MacBook-Pro.local>
2017-05-14 12:35:22 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Blog.create!([
+  {title: "test 1", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam venenatis orci quis nunc varius, pellentesque porttitor leo blandit. Fusce vitae imperdiet ante. Cras elementum a turpis et placerat. In hac habitasse platea dictumst. Suspendisse potenti. Quisque tincidunt odio dui, varius porttitor turpis pellentesque ut. Mauris maximus orci eget tellus consequat, in commodo elit laoreet. Donec quis ullamcorper tortor, et laoreet ipsum. Fusce egestas ipsum vel augue sodales, a lacinia urna fringilla. Curabitur aliquam, enim et posuere vulputate, augue neque accumsan eros, quis tempor nisl metus quis risus. Nunc tempus nulla non eros semper finibus. Cras aliquam elit in justo euismod fringilla."},
+  {title: "Test 2", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam venenatis orci quis nunc varius, pellentesque porttitor leo blandit. Fusce vitae imperdiet ante. Cras elementum a turpis et placerat. In hac habitasse platea dictumst. Suspendisse potenti. Quisque tincidunt odio dui, varius porttitor turpis pellentesque ut. Mauris maximus orci eget tellus consequat, in commodo elit laoreet. Donec quis ullamcorper tortor, et laoreet ipsum. Fusce egestas ipsum vel augue sodales, a lacinia urna fringilla. Curabitur aliquam, enim et posuere vulputate, augue neque accumsan eros, quis tempor nisl metus quis risus. Nunc tempus nulla non eros semper finibus. Cras aliquam elit in justo euismod fringilla."}
+])
+Portfolio.create!([
+  {name: "test 1", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 2", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 3", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 4", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 5", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 6", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 7", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 8", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"},
+  {name: "test 9", client: "", completed: "2017-05-14", about: "", category_id: nil, skill_id: nil, long_landscape: "5.jpg", long_potrait: "1.jpg", square: "2.jpg"}
+])
